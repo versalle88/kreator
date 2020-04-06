16cab7e338bb5ab3c6cb5d003528fd075a938610
Andrew Versalle <andrew.versalle@gmail.com>
2020-04-06 18:22:14 -0400

    Made DI config overridable

--- boot/container.php
+++ boot/container.php
@@ -4,6 +4,14 @@ declare(strict_types=1);
 
 use Versalle\Container\Container;
 
-$config = include CONFIG_DIR . DIRECTORY_SEPARATOR . 'di.php';
+$config            = include CONFIG_DIR . DIRECTORY_SEPARATOR . 'di.php';
+$applicationConfig = include APP_DIR . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'di.php';
+
+foreach ($applicationConfig['objects'] as $key => $object) {
+    $config['objects'][$key] = array_merge($config['objects'][$key], $object);
+}
+foreach ($applicationConfig['parameters'] as $key => $object) {
+    $config['parameters'][$key] = array_merge($config['parameters'][$key], $object);
+}
 
 return new Container($config['objects'], $config['parameters']);
