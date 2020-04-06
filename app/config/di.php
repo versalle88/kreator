bf7f8c6332a65fa7e7aa262c00e45a1318459af7
Andrew Versalle <andrew.versalle@gmail.com>
2020-04-06 17:47:51 -0400

    Created application DI config file

---
+++ app/config/di.php
@@ -0,0 +1,13 @@
+<?php /** @noinspection PhpFullyQualifiedNameUsageInspection */
+
+declare(strict_types=1);
+
+use Versalle\Container\Entry\ObjectEntry;
+use Versalle\Container\Entry\ParameterEntry;
+
+return [
+    'objects' => [
+    ],
+    'parameters' => [
+    ],
+];
