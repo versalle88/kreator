4826d22c9622af28e3bea17361019ede733ee186
Andrew Versalle <andrew.versalle@gmail.com>
2020-04-06 18:09:14 -0400

    Made routes config overridable

--- boot/constants.php
+++ boot/constants.php
@@ -4,5 +4,6 @@ declare(strict_types=1);
 
 define('ROOT_DIR', dirname(__DIR__));
 
+define('APP_DIR', ROOT_DIR . DIRECTORY_SEPARATOR . 'app');
 define('CONFIG_DIR', ROOT_DIR . DIRECTORY_SEPARATOR . 'config');
 define('VENDOR_DIR', ROOT_DIR . DIRECTORY_SEPARATOR . 'vendor');
--- boot/dispatcher.php
+++ boot/dispatcher.php
@@ -8,7 +8,13 @@ use function FastRoute\simpleDispatcher;
 
 return simpleDispatcher(
     function (RouteCollector $r) {
-        $routes = include CONFIG_DIR . DIRECTORY_SEPARATOR . 'routes.php';
+        $routes            = include CONFIG_DIR . DIRECTORY_SEPARATOR . 'routes.php';
+        $applicationRoutes = include APP_DIR . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'routes.php';
+
+        foreach ($applicationRoutes as $key => $item) {
+            $routes[$key] = array_merge($routes[$key], $item);
+        }
+
         foreach ($routes as $route => $item) {
             foreach ($item as $httpMethod => $handler) {
                 $r->addRoute(
