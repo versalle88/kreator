d99bc3b7e678980adaea203bf6340429ab898409
Andrew Versalle <andrew.versalle@gmail.com>
2020-04-08 17:25:47 -0400

    Added try/catch to entry point file

--- public/index.php
+++ public/index.php
@@ -2,4 +2,10 @@
 
 declare(strict_types=1);
 
-require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'boot' . DIRECTORY_SEPARATOR . 'bootstrap.php';
+try {
+    require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'boot' . DIRECTORY_SEPARATOR . 'bootstrap.php';
+} catch (Exception $e) {
+    header($_SERVER['SERVER_PROTOCOL'] . ' 503 Service Unavailable.', TRUE, 503);
+    echo $e->getMessage();
+    exit(1);
+}
