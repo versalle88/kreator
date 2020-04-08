ab0df3fd3fb49a490332c79d6414c87baed9c430
Andrew Versalle <andrew.versalle@gmail.com>
2020-04-08 17:30:32 -0400

    Refactored autoload to check for Composer autoload file

--- boot/autoload.php
+++ boot/autoload.php
@@ -2,4 +2,11 @@
 
 declare(strict_types=1);
 
-require VENDOR_DIR . DIRECTORY_SEPARATOR . 'autoload.php';
+$filename = VENDOR_DIR . DIRECTORY_SEPARATOR . 'autoload.php';
+
+if (!file_exists($filename)) {
+    throw new RuntimeException('Composer autoload file not found. Please run `composer install`.');
+}
+
+/** @noinspection PhpIncludeInspection */
+include $filename;
