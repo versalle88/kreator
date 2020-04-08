7f6f119448c8ac6c1d9132555cb7aee66bddc33e
Andrew Versalle <andrew.versalle@gmail.com>
2020-04-08 18:23:13 -0400

    Refactored booting of `Kernel`

--- src/Kernel.php
+++ src/Kernel.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace Versalle\Framework;
 
+use Versalle\Framework\Container\ContainerFactory;
+
 final class Kernel
 {
+    private $containerFactory;
+
     private static $instance;
 
     /**
@@ -13,19 +17,33 @@ final class Kernel
      *
      * is not allowed to call from outside to prevent from creating multiple instances,
      * to use the singleton, you have to obtain the instance from Singleton::getInstance() instead
+     *
+     * @param ContainerFactory $containerFactory
      */
-    private function __construct()
+    private function __construct(ContainerFactory $containerFactory)
     {
-
+        $this->containerFactory = $containerFactory;
     }
 
     public static function boot(): Kernel
     {
         if (is_null(static::$instance)) {
-            static::$instance = new static();
+            static::$instance = static::createInstance();
         }
 
         return static::$instance;
+    }
+
+    private static function createInstance(): Kernel
+    {
+        $containerFactory = static::createContainerFactory();
+
+        return new static($containerFactory);
+    }
+
+    private static function createContainerFactory(): ContainerFactory
+    {
+        return new ContainerFactory();
     }
 
     /**
