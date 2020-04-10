2abc298f909626c38dc625fab1d5dc1b4175a0cf
Andrew Versalle <andrew.versalle@gmail.com>
2020-04-10 14:24:02 -0400

    Fixed typo

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ use Versalle\Framework\Kernel;
 try {
     require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'boot' . DIRECTORY_SEPARATOR . 'bootstrap.php';
 } catch (Exception $e) {
-    header($_SERVER['SERVER_PROTOCOL'] . ' 503 Service Unavailable.', TRUE, 503);
+    header($_SERVER['SERVER_PROTOCOL'] . ' 503 Service Unavailable.', true, 503);
     echo $e->getMessage();
     exit(1);
 }
