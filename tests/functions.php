2d16c7759c10e0f973070680f83d11a97f2eaacc
Andrew Versalle <andrew.versalle@gmail.com>
2020-04-10 14:45:49 -0400

    Created tests functions file

---
+++ tests/functions.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use Psr\Container\ContainerInterface;
+use Versalle\Framework\Container\ContainerFactory;
+use Versalle\Framework\FileSystem\DirectoryList;
+
+function _get($id)
+{
+    $container = _container();
+
+    return $container->get($id);
+}
+
+function _container(): ContainerInterface
+{
+    $containerFactory = _containerFactory();
+
+    return $containerFactory->create();
+}
+
+function _containerFactory(): ContainerFactory
+{
+    return new ContainerFactory(new DirectoryList(ROOT_DIR));
+}
