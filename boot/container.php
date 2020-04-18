5f92ff9f74e2df7fa0177ab24749cac59388e1f0
Andrew Versalle <andrew.versalle@gmail.com>
2020-04-18 14:53:41 -0400

    Refactored `container.php` to use `ContainerFactory`

--- boot/container.php
+++ boot/container.php
@@ -2,16 +2,7 @@
 
 declare(strict_types=1);
 
-use Versalle\Container\Container;
+use Versalle\Framework\Container\ContainerFactory;
+use Versalle\Framework\FileSystem\DirectoryList;
 
-$config            = include CONFIG_DIR . DIRECTORY_SEPARATOR . 'di.php';
-$applicationConfig = include APP_DIR . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'di.php';
-
-foreach ($applicationConfig['objects'] as $key => $object) {
-    $config['objects'][$key] = array_merge($config['objects'][$key], $object);
-}
-foreach ($applicationConfig['parameters'] as $key => $object) {
-    $config['parameters'][$key] = array_merge($config['parameters'][$key], $object);
-}
-
-return new Container($config['objects'], $config['parameters']);
+return (new ContainerFactory(new DirectoryList(ROOT_DIR)))->create();
