ad8d3f5cf95747b28bfa09d6256a07bea7f8529f
Andrew Versalle <andrew.versalle@gmail.com>
2020-05-04 08:28:02 -0400

    Used framework environment to configure debugger

--- boot/debugger.php
+++ boot/debugger.php
@@ -4,4 +4,13 @@ declare(strict_types=1);
 
 use Tracy\Debugger;
 
-Debugger::enable();
+switch (FRAMEWORK_ENVIRONMENT) {
+    case 'development':
+    case 'testing':
+        Debugger::enable(Debugger::DEVELOPMENT);
+        break;
+    case 'staging':
+    case 'production':
+        Debugger::enable(Debugger::PRODUCTION);
+        break;
+}
