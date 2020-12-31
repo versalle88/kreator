c99ab0794290c0c4a1fd53f2c461e879823c2be5
Andrew Versalle <andrew.versalle@gmail.com>
2020-12-31 14:48:17 -0500

    Created `NikicFastRouteRouter` and spec

---
+++ src/Application/NikicFastRouteRouter.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Versalle\Framework\Application;
+
+use FastRoute\Dispatcher;
+use FastRoute\RouteCollector;
+use Psr\Http\Message\RequestInterface;
+use Versalle\Framework\FileSystem\DirectoryList;
+
+use function FastRoute\simpleDispatcher;
+
+final class NikicFastRouteRouter implements RouterInterface
+{
+    private $directoryList;
+
+    public function __construct(DirectoryList $directoryList)
+    {
+        $this->directoryList = $directoryList;
+    }
+
+    public function match(RequestInterface $request): array
+    {
+        $dispatcher = $this->createDispatcher();
+
+        return $dispatcher->dispatch(
+            $request->getMethod(),
+            $request->getRequestTarget()
+        );
+    }
+
+    private function createDispatcher(): Dispatcher
+    {
+        return simpleDispatcher(
+            function (RouteCollector $routeCollector) {
+                $routes = $this->loadRoutes();
+
+                foreach ($routes as $route => $item) {
+                    foreach ($item as $httpMethod => $handler) {
+                        $routeCollector->addRoute(
+                            $httpMethod,
+                            $route,
+                            $handler
+                        );
+                    }
+                }
+            }
+        );
+    }
+
+    private function loadRoutes(): array
+    {
+        $frameworkRoutes   = $this->loadFrameworkRoutes();
+        $applicationRoutes = $this->loadApplicationRoutes();
+
+        foreach ($applicationRoutes as $key => $item) {
+            $frameworkRoutes[$key] = array_merge($frameworkRoutes[$key] ?? [], $item);
+        }
+
+        return $frameworkRoutes;
+    }
+
+    private function loadFrameworkRoutes(): array
+    {
+        /** @noinspection PhpIncludeInspection */
+        return include $this->directoryList->getFrameworkConfigDir() . 'routes.php';
+    }
+
+    private function loadApplicationRoutes(): array
+    {
+        /** @noinspection PhpIncludeInspection */
+        return include $this->directoryList->getApplicationConfigDir() . 'routes.php';
+    }
+}
---
+++ tests/spec/Application/NikicFastRouteRouterSpec.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @noinspection PhpIllegalPsrClassPathInspection
+ * @noinspection PhpUnused
+ */
+
+namespace spec\Versalle\Framework\Application;
+
+use PhpSpec\ObjectBehavior;
+use Psr\Http\Message\RequestInterface;
+use Versalle\Framework\Application\NikicFastRouteRouter;
+use Versalle\Framework\Application\RouterInterface;
+use Versalle\Framework\FileSystem\DirectoryList;
+
+class NikicFastRouteRouterSpec extends ObjectBehavior
+{
+    function let()
+    {
+        $this->beConstructedWith(new DirectoryList(FRAMEWORK_ROOT_DIR));
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(NikicFastRouteRouter::class);
+    }
+
+    function it_implements_application_interface()
+    {
+        $this->shouldImplement(RouterInterface::class);
+    }
+
+    function it_matches_a_request_to_a_route(RequestInterface $request)
+    {
+        $this->match($request)
+            ->shouldBeArray();
+    }
+}
